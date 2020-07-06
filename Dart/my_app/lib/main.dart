4620b749dcb273eabca860e85c9c01833fb21656
mario5698 <arancibiagm@gmail.com>
2020-07-06 16:52:41 +0200

    Clean APP

--- Dart/my_app/lib/main.dart
+++ Dart/my_app/lib/main.dart
@@ -1,41 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/tabs/inicio_tabs.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final TabController = new DefaultTabController(
-      length: 5, //numero iconos  de barra
-      child: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('navegacion por Tabs'),
-          bottom: new TabBar(
-            indicatorColor: Colors.red,
-            tabs: <Widget>[
-              new Tab(icon: new Icon(Icons.home), text: 'inicio'),
-              new Tab(icon: new Icon(Icons.contacts), text: 'registro'),
-              new Tab(icon: new Icon(Icons.place), text: 'lugares'),
-              new Tab(icon: new Icon(Icons.help), text: 'ayuda'),
-            ],
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
           ),
         ),
-
-body: new TabBarView(
-  children: <Widget>[
-new inicioTabs (),
-  ],
-),
-
       ),
     );
-    return new MaterialApp(
-      title: 'Tabs con Flutter ',
-    theme: new ThemeData(
-primarySwatch: Colors.blue,
-    )
-  home: TabController,
-    )
   }
 }
