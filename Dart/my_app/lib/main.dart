077132bd355b2b2da7e2f092f3576a1bcf1841dc
mario5698 <arancibiagm@gmail.com>
2020-07-06 16:52:04 +0200

    Clean APP

--- Dart/my_app/lib/main.dart
+++ Dart/my_app/lib/main.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/tabs/inicio_tabs.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Material App',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Material App Bar'),
-            ),
-            body: Row(children: <Widget>[
-              cuadrado(Colors.pink),
-              Expanded(child: cuadrado(Colors.green)),
-              Expanded(flex: 2, child: cuadrado(Colors.green)),
-              cuadrado(Colors.pink),
-            ])));
-  }
-}
+    final TabController = new DefaultTabController(
+      length: 5, //numero iconos  de barra
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('navegacion por Tabs'),
+          bottom: new TabBar(
+            indicatorColor: Colors.red,
+            tabs: <Widget>[
+              new Tab(icon: new Icon(Icons.home), text: 'inicio'),
+              new Tab(icon: new Icon(Icons.contacts), text: 'registro'),
+              new Tab(icon: new Icon(Icons.place), text: 'lugares'),
+              new Tab(icon: new Icon(Icons.help), text: 'ayuda'),
+            ],
+          ),
+        ),
 
-class cuadrado extends StatelessWidget {
-  final Color color;
-  cuadrado(this.color);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-      width: 50.0,
-      height: 50.0,
-      decoration: BoxDecoration(color: this.color),
+body: new TabBarView(
+  children: <Widget>[
+new inicioTabs (),
+  ],
+),
+
+      ),
     );
+    return new MaterialApp(
+      title: 'Tabs con Flutter ',
+    theme: new ThemeData(
+primarySwatch: Colors.blue,
+    )
+  home: TabController,
+    )
   }
 }
