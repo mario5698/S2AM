d6558a7782f48cf7171184219ef65a8f5dd06433
mario5698 <arancibiagm@gmail.com>
2020-07-07 19:55:35 +0200

    test App Flutter

--- Dart/my_app/lib/main.dart
+++ Dart/my_app/lib/main.dart
@@ -6,16 +6,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+      title: "My_app",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  Inicio({Key key}) : super(key: key);
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("test"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+            child: Text(
+              'Data',
+              textAlign: Center,
+            ),
           ),
-        ),
+          Text("test"),
+        ],
       ),
     );
   }
