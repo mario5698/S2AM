ddafcd7b7cf2aa8e524be749cd2e5c2d234817d1
mario5698 <arancibiagm@gmail.com>
2020-07-14 19:54:25 +0200

    test python

---
+++ Aprender python/UF-1-Resuelto/ICB0M03U01I03/act1.py
@@ -0,0 +1,13 @@
+
+print ("Modul 1 :")
+numero1=int( input())
+print ("Modul 2 :")
+numero2=int( input())
+print ("Modul 3 :")
+numero3=int( input())
+print ("Modul 4 :")
+numero4= int( input())
+
+suma  = numero1+numero2+numero3+numero4
+
+print( suma )
