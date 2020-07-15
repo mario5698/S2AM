1a20d8999cf294229a30e1c0cf19aa4d589de399
mario5698 <arancibiagm@gmail.com>
2020-07-15 18:48:44 +0200

    new Folder

---
+++ Aprender python/UF-1-Resuelto/ICB0M03U01I03/act5.py
@@ -0,0 +1,9 @@
+print("Modul 1 :")
+modul1=int(input ())
+print("HORES MODUL 1 :")
+hores_modul1=int(input ())
+print("Modul 2 :")
+modul2=int(input ())
+print("HORES MODUL 2 :")
+hores_modul2=int(input ())
+
