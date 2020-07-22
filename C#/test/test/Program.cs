d4499f0c1904fd2182dbe3f0e4d3a1cf811de748
mario5698 <arancibiagm@gmail.com>
2020-07-22 18:11:04 +0200

    new Folder

---
+++ C#/test/test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test
+{
+    class MainClass
+    {
+        public static void Main(string[] args){
+
+            int km, preu;
+            double preuxkm;
+
+            Console.WriteLine("KM :");
+
+            km = Console.ReadLine();
+
+            Console.WriteLine("Preu Per KM  :");
+
+            preu = Console.ReadLine();
+
+
+            preuxkm = km * preu;
+
+            Console.WriteLine("Preu: " + preuxkm);
+                }
+
+       
+    }
+}
