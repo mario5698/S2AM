63d8fd5382ec1a188f304a1542325013aee2c297
mario5698 <arancibiagm@gmail.com>
2020-07-27 14:30:21 +0200

    new Folder

--- Dart/my_app/lib/main.dart
+++ Dart/my_app/lib/main.dart
@@ -1,41 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
 
-class MyApp extends StatelessWidget {
+  // Los campos en una subclase de Widgets siempre están marcados como "final".
+
+  final Widget title;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My_app",
-      home: Inicio(),
+    return Container(
+      height: 56.0, // en píxeles lógicos
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      // Row es un diseño horizontal y lineal.
+      child: Row(
+        // <Widget> es el tipo de artículos en la lista.
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null desactiva el botón
+          ),
+          // Expanded expande a su hijo para rellenar el espacio disponible.
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
     );
   }
 }
 
-class Inicio extends StatefulWidget {
-  Inicio({Key key}) : super(key: key);
-  @override
-  _InicioState createState() => _InicioState();
-}
-
-class _InicioState extends State<Inicio> {
+class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("test"),
-        ),
-        body: Row(children: <Widget>[
-          Text(" Hello "),
-          Center(
-            child: Container(
-              margin: const EdgeInsets.all(10.0),
-              color: Colors.amber[600],
-              width: 48.0,
-              height: 48.0,
+    // Material es una hoja de papel conceptual en la que aparece la UI.
+    return Material(
+      // Column es un diseño vertical y lineal.
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.title,
             ),
           ),
-          Text(" Word")
-        ]));
+          Expanded(
+            child: Center(
+              child: Text('Hello, world!'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title:
+        'My app', // utilizado por el conmutador de tareas del sistema operativo
+    home: MyScaffold(),
+  ));
 }
