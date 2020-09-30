a5e855d14e4ec7fa63c9af1c1fa6f28e2bca55c8
mario5698 <arancibiagm@gmail.com>
2020-09-30 17:23:04 +0200

    first project using c#

---
+++ Code/C#/Messi/Messi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Messi
+{
+    public partial class Form1 : Form
+    {
+        int number = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled=true;
+            timer1.Interval = 300;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            number = number + 1;
+            if (number == 100){
+                Form f2 = new Form();
+                f2.Show();
+                timer1.Enabled =false;
+                this.Close();
+            }
+            progressBar1.Value = number;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Code/C#/SolucioProva/SolucioProva/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SolucioProva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_url = new System.Windows.Forms.Label();
+            this.txt_label_desc = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btm_send = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel1.Controls.Add(this.txt_url);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(660, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btm_send);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.txt_label_desc);
+            this.panel2.Location = new System.Drawing.Point(2, 96);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(660, 203);
+            this.panel2.TabIndex = 1;
+            // 
+            // txt_url
+            // 
+            this.txt_url.AutoSize = true;
+            this.txt_url.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_url.Location = new System.Drawing.Point(112, 21);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(399, 44);
+            this.txt_url.TabIndex = 0;
+            this.txt_url.Text = "VivendaBarateta.com";
+            // 
+            // txt_label_desc
+            // 
+            this.txt_label_desc.AutoSize = true;
+            this.txt_label_desc.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_label_desc.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txt_label_desc.Location = new System.Drawing.Point(10, 0);
+            this.txt_label_desc.Name = "txt_label_desc";
+            this.txt_label_desc.Size = new System.Drawing.Size(146, 16);
+            this.txt_label_desc.TabIndex = 0;
+            this.txt_label_desc.Text = "Descripcio Vivenda";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(421, 130);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(483, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 130);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btm_send
+            // 
+            this.btm_send.Location = new System.Drawing.Point(13, 172);
+            this.btm_send.Name = "btm_send";
+            this.btm_send.Size = new System.Drawing.Size(75, 23);
+            this.btm_send.TabIndex = 3;
+            this.btm_send.Text = "Enviar";
+            this.btm_send.UseVisualStyleBackColor = true;
+            this.btm_send.Click += new System.EventHandler(this.btm_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 301);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label txt_url;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txt_label_desc;
+        private System.Windows.Forms.Button btm_send;
+    }
+}
+
