56a80446e1552a82ed3e026a9208f909def5779e
mario5698 <arancibiagm@gmail.com>
2020-11-04 08:07:36 +0100

    advance

---
+++ Code/Flutter/peliculas-flutter -ok/lib/src/widgets/card_swiper_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/models/actores_model.dart';
+import '../models/actores_model.dart';
+
+class CardSwiper extends StatelessWidget {
+  //final List<Pelicula> peliculas;
+  final List<Actor> Actores;
+
+  CardSwiper({@required this.Actores});
+
+  @override
+  Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
+        itemBuilder: (BuildContext context, int index) {
+          //Actores[index].castId = '${Actores[index]-tarjeta}';
+
+          return Hero(
+            tag: Actores[index].id,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: GestureDetector(
+                  onTap: () => Navigator.pushNamed(context, 'detalle',
+                      arguments: Actores[index]),
+                  child: FadeInImage(
+                    image: NetworkImage(Actores[index].getFoto()),
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                )),
+          );
+        },
+        itemCount: Actores.length,
+        // pagination: new SwiperPagination(),
+        // control: new SwiperControl(),
+      ),
+    );
+  }
+}
