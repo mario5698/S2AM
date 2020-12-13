7eba60cccad2ee7992f470a62bcc703de80230e4
mario5698 <arancibiagm@gmail.com>
2020-12-13 23:20:43 +0100

    create new project

---
+++ Code/Flutter/sqlite_and_mock/lib/pages/sqlite/sqlite_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Sqlite_Page extends StatelessWidget {
+  const Sqlite_Page({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('sqlite page'),
+    );
+  }
+}
