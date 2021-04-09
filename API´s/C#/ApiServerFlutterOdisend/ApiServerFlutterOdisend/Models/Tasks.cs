ab04b94e2c22bb3552b416ca3b93bdab5c8cee85
mario5698 <aranicbiagm@gmail.com>
2021-04-09 18:08:48 +0200

    New Api Updated 09/04/2021

--- API´s/C#/ApiServerFlutterOdisend/ApiServerFlutterOdisend/Models/Tasks.cs
+++ API´s/C#/ApiServerFlutterOdisend/ApiServerFlutterOdisend/Models/Tasks.cs
@@ -8,12 +8,20 @@ namespace ApiServerFlutterOdisend.Models
     public class Tasks
     {
         public int Id { get; set; }
-        public string Direction_Delivery { get; set; }
-        public string Direction_Pickup { get; set; }
+        public string DirectionDelivery { get; set; }
+        public string DirectionPickup { get; set; }
         public string Content { get; set; }
         public int UserID { get; set; }
         public int RiderID { get; set; }
-        public bool Asigned { get; set; }
+        public string TokenPickup { get; set; }
+        public string TokenDelivery { get; set; }
+        public string LngPickup { get; set; }
+        public string LatPickup { get; set; }
+        public string LngDelivery { get; set; }
+        public string LatDelivery { get; set; }
+        public double KM { get; set; }
+        public double KG { get; set; }
         public string State { get; set; }
+        
     }
 }
